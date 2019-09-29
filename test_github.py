87213e3eb617a540ec71c409e65aeb6943f9788c
Tanvi <thanamsh@steven.edu>
2019-09-29 18:15:53 -0400

    after mock

--- test_github.py
+++ test_github.py
@@ -5,14 +5,24 @@ Script:  Test github script
 
 
 import unittest
+from unittest import mock
 from github import github_api
+import github
+
 
 
 class TestGithub(unittest.TestCase):
-   
+    @mock.patch('github.github_api', side_effect=github_api)
+    def test_github_api(self, github_api_function):
+        assert github_api("Tanvi-412") == "Repository Name: 567-HW01, Total number of commits: 2"
+        result = github.github_api("Tanvi-412")
+        print(result)
 
     def testgithub(self): 
         self.assertEqual(github_api("Tanvi-412"),"Repository Name: 567-HW01, Total number of commits: 2")
+        result = github.github_api("Tanvi-412")
+        print(result)
+
         
 
 
